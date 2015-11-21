e26f19462865a91001c79b3f695a38200a5f98a0
ADJA GOSSAN <adjagossan@gmail.com>
2015-11-21 16:48:02 +0100

    class PressePapier created

---
+++ src/receiver/PressePapier.java
@@ -0,0 +1,28 @@
+package receiver;
+
+public class PressePapier {
+	
+	private String contenu;
+
+	private IMoteurEdition moteurEdition;
+	
+	public PressePapier(){
+		this.contenu = "";
+	}
+
+	public IMoteurEdition getMoteurEdition() {
+		return moteurEdition;
+	}
+
+	public void setMoteurEdition(IMoteurEdition moteurEdition) {
+		this.moteurEdition = moteurEdition;
+	}
+	
+	public String getContenu() {
+		return contenu;
+	}
+
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+}
