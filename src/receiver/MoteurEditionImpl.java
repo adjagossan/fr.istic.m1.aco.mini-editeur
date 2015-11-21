883896c668168e74a90dc214bddcbdf0126830f5
ADJA GOSSAN <adjagossan@gmail.com>
2015-11-21 16:28:34 +0100

    class MoteurEditionImpl created and implements IMoteurEdition

---
+++ src/receiver/MoteurEditionImpl.java
@@ -0,0 +1,29 @@
+package receiver;
+
+public class MoteurEditionImpl implements IMoteurEdition {
+
+	@Override
+	public void couper() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void copier() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void coller() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void saisir(String texte) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
