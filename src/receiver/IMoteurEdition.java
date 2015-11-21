0cad842e41e88b3276f7f1ec278d67efb417efa6
ADJA GOSSAN <adjagossan@gmail.com>
2015-11-21 16:24:44 +0100

    interface IMoteurEdition created

---
+++ src/receiver/IMoteurEdition.java
@@ -0,0 +1,13 @@
+package receiver;
+
+public interface IMoteurEdition {
+	void couper();
+
+	void copier();
+
+	void coller();
+
+	//void selectionner(Selection selection);
+
+	void saisir(String texte);
+}
