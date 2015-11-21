3c9fb9316f6e025c8370acb527791020786a0306
ADJA GOSSAN <adjagossan@gmail.com>
2015-11-21 16:42:23 +0100

    class Buffer created

---
+++ src/receiver/Buffer.java
@@ -0,0 +1,32 @@
+package receiver;
+
+public class Buffer {
+	
+	private StringBuffer contenu;
+
+	private IMoteurEdition moteurEdition;
+
+	public IMoteurEdition getMoteurEdition() {
+		return moteurEdition;
+	}
+
+	private void setMoteurEdition(IMoteurEdition moteurEdition) {
+		this.moteurEdition = moteurEdition;
+	}
+
+	public Buffer () {
+		contenu = new StringBuffer();
+	}
+
+	public StringBuffer getContenu() {
+		return contenu;
+	}
+
+	public void delete(int indiceDebut, int indiceFin){
+		contenu.delete(indiceDebut, indiceFin);
+	}
+
+	public void insert(int indiceDebut, String texte  ){
+		contenu.insert(indiceDebut, texte);
+	}
+}
