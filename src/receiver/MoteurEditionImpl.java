dd59f1d4af5cf36da2d81a99ff7f89fd45e3943c
ADJA GOSSAN <adjagossan@gmail.com>
2015-11-22 02:11:38 +0100

    MoteurEdition's constructor added

--- src/receiver/MoteurEditionImpl.java
+++ src/receiver/MoteurEditionImpl.java
@@ -2,6 +2,16 @@ package receiver;
 
 public class MoteurEditionImpl implements IMoteurEdition {
 
+	private Buffer buffer;
+	private PressePapier pressePapier;
+	private Selection selection;
+
+	public MoteurEditionImpl () {
+		pressePapier = new PressePapier();
+		buffer = new Buffer();
+		selection = new Selection(0,0);
+	}
+
 	@Override
 	public void couper() {
 		// TODO Auto-generated method stub
@@ -22,6 +32,12 @@ public class MoteurEditionImpl implements IMoteurEdition {
 
 	@Override
 	public void saisir(String texte) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void selectionner(Selection selection) {
 		// TODO Auto-generated method stub
 
 	}
