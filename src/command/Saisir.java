f478a7f550a460a3d5d3d9f4c99fadcdcc24d5cb
ADJA GOSSAN <adjagossan@gmail.com>
2015-11-22 06:12:35 +0100

    command Saisir implemented

---
+++ src/command/Saisir.java
@@ -0,0 +1,23 @@
+package command;
+
+import invoker.IHM;
+import receiver.IMoteurEdition;
+
+public class Saisir implements ICommand {
+
+	private IMoteurEdition mMoteurEdition; 
+	private IHM ihm;
+	
+	public Saisir(IMoteurEdition mMoteurEdition, IHM ihm){
+			this.mMoteurEdition = mMoteurEdition;
+			this.ihm = ihm;
+	}
+	
+	@Override
+	public void execute() {
+		String inputCharacter = ihm.getInputCharacter();
+		mMoteurEdition.saisir(inputCharacter);
+		
+	}
+
+}
