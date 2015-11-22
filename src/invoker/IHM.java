65dd8e27913f08755f1407a81a05f3e1cefcda49
ADJA GOSSAN <adjagossan@gmail.com>
2015-11-22 07:02:29 +0100

    class IHM updated

--- src/invoker/IHM.java
+++ src/invoker/IHM.java
@@ -1,21 +1,41 @@
 package invoker;
 
+import java.util.HashMap;
+
 import receiver.Selection;
 
+import command.ICommand;
+/**
+ *  invoker for the Command design pattern
+ * 
+ *
+ */
 public class IHM {
 	
 	private Selection selection;
 	private String inputCharacter;
-
+	private HashMap<String, ICommand> cmds = null;
+	/**
+	 * constructs the invoker IHM with an empty HashMap<String, ICommand>
+	 */
 	public IHM(){
 		selection = new Selection(0,0);
 		inputCharacter = "";
+		cmds = new HashMap<>();
+		
 	}
-	
+	/**
+	 * returns the property selection of this object
+	 * @return non-null Selection reference
+	 */
 	public Selection getSelection(){
 		return selection;
 	}
-	
+	/**
+	 * set the property selection of this object 
+	 * @param deb the begining index
+	 * @param fin the ending index
+	 */
 	public void setSelction(int deb, int fin){
 		selection.setSelection(deb, fin);
 	}
@@ -26,5 +46,19 @@ public class IHM {
 
 	public void setInputCharacter(String inputCharacter) {
 		this.inputCharacter = inputCharacter;
+	}
+	
+	public void addCommand(String keyword, ICommand cmd) {
+        if ((keyword == null) || (cmd == null)) {
+            throw new IllegalArgumentException("null parameter");
+        }
+        cmds.put(keyword,cmd);
+    }
+	
+	public void invoke(String cmd){
+		if (cmds.containsKey(cmd))
+			cmds.get(cmd).execute();
+		else
+			throw new IllegalArgumentException(cmd+" isn't a valid command");
 	}
 }
