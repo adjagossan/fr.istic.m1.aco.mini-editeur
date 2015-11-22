0611704861e936d86fa1da2b3ef802ad72f941a7
ADJA GOSSAN <adjagossan@gmail.com>
2015-11-22 05:42:00 +0100

    class IHM updated

--- src/invoker/IHM.java
+++ src/invoker/IHM.java
@@ -1,5 +1,30 @@
-package ihm;
+package invoker;
+
+import receiver.Selection;
 
 public class IHM {
+	
+	private Selection selection;
+	private String inputCharacter;
 
+	public IHM(){
+		selection = new Selection(0,0);
+		inputCharacter = "";
+	}
+	
+	public Selection getSelction(){
+		return selection;
+	}
+	
+	public void setSelction(int deb, int fin){
+		selection.setSelection(deb, fin);
+	}
+	
+	public String getInputCharacter() {
+		return inputCharacter;
+	}
+
+	public void setInputCharacter(String inputCharacter) {
+		this.inputCharacter = inputCharacter;
+	}
 }
