4875a965b5b173b1d40d21544f47a634a4939b4b
ADJA GOSSAN <adjagossan@gmail.com>
2015-11-24 21:58:22 +0100

    class Saisir updated

--- src/command/Saisir.java
+++ src/command/Saisir.java
@@ -8,19 +8,19 @@ public class Saisir implements ICommand {
 
 	private IMoteurEdition mMoteurEdition; 
 	private IHM ihm;
-	/*private IEnregistreur mEnregistreur;*/
+	private IEnregistreur mEnregistreur;
 	
-	public Saisir(IMoteurEdition mMoteurEdition, IHM ihm/*, IEnregistreur mEnregistreur*/){
+	public Saisir(IMoteurEdition mMoteurEdition, IHM ihm, IEnregistreur mEnregistreur){
 			this.mMoteurEdition = mMoteurEdition;
 			this.ihm = ihm;
-			/*this.mEnregistreur = mEnregistreur;*/
+			this.mEnregistreur = mEnregistreur;
 	}
 	
 	
 	public void execute() {
 		String inputCharacter = ihm.getInputCharacter();
 		mMoteurEdition.saisir(inputCharacter);
-		/*mEnregistreur.enregistrer(this);*/
+		mEnregistreur.enregistrer(this);
 	}
 
 }
