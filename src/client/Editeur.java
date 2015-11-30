a192b540db1c58b189fa1b5050369b1bf051b144
ADJA GOSSAN <adjagossan@gmail.com>
2015-11-30 03:32:28 +0100

    class Editeur updated

--- src/client/Editeur.java
+++ src/client/Editeur.java
@@ -12,15 +12,25 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import receiver.EnregistreurImpl;
+import receiver.IEnregistreur;
 import receiver.IMoteurEdition;
 import receiver.MoteurEditionImpl;
 
+import command.Arreter;
 import command.Coller;
+import command.CollerEnreg;
 import command.Copier;
+import command.CopierEnreg;
 import command.Couper;
+import command.CouperEnreg;
+import command.Demarrer;
 import command.ICommand;
+import command.Rejouer;
 import command.Saisir;
+import command.SaisirEnregistreur;
 import command.Selectionner;
+import command.SelectionnerEnreg;
 
 
 public class Editeur extends JFrame {
@@ -30,6 +40,9 @@ public class Editeur extends JFrame {
 	private JButton JBCopier = null;
 	private JButton JBCouper = null;
 	private JButton JBColler = null;
+	private JButton JBDemarrer;
+	private JButton JBArreter;
+	private JButton JBRejouer;
 	
 	/**
 	 * @param args
@@ -37,21 +50,35 @@ public class Editeur extends JFrame {
 	public static void main(String[] args) {
 		
 		IMoteurEdition moteur = new MoteurEditionImpl();
+		IEnregistreur enregistreur = new EnregistreurImpl();
 		
 		IHM ihm = new IHM();
 		moteur.register(ihm);
 		
-		ICommand copier = new Copier(moteur);
+		/*ICommand copier = new Copier(moteur);
 		ICommand couper = new Couper(moteur);
 		ICommand coller = new Coller(moteur);
 		ICommand selectionner = new Selectionner(moteur, ihm);
-		ICommand saisir = new Saisir(moteur, ihm);
+		ICommand saisir = new Saisir(moteur, ihm);*/
+		
+		ICommand copier = new CopierEnreg(moteur, enregistreur);
+		ICommand couper = new CouperEnreg(moteur, enregistreur);
+		ICommand coller = new CollerEnreg(moteur, enregistreur);
+		ICommand selectionner = new SelectionnerEnreg(moteur, enregistreur, ihm);
+		ICommand saisir = new SaisirEnregistreur(moteur, enregistreur, ihm);
+		
+		ICommand demarrer = new Demarrer(enregistreur);
+		ICommand arreter = new Arreter(enregistreur);
+		ICommand rejouer = new Rejouer(enregistreur);
 
 		ihm.addCommand("couper", couper);
 		ihm.addCommand("coller", coller);
 		ihm.addCommand("copier", copier);
 		ihm.addCommand("selectionner", selectionner);
 		ihm.addCommand("saisir", saisir);
+		ihm.addCommand("demarrer", demarrer);
+		ihm.addCommand("rejouer", rejouer);
+		ihm.addCommand("arreter", arreter);
 
 		Editeur editeur = new Editeur(moteur, ihm);
 	}
@@ -108,9 +135,39 @@ public class Editeur extends JFrame {
 			}
 		});
 		
+		JBDemarrer = new JButton("Demarrer");
+		JBDemarrer.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ihm.invoke("demarrer");
+			}	
+		});
+		
+		JBArreter = new JButton("Arreter");
+		JBArreter.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ihm.invoke("arreter");
+			}	
+		});
+		
+		JBRejouer = new JButton("Rejouer");
+		JBRejouer.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ihm.invoke("rejouer");
+			}	
+		});
+		
 		buttonPanel.add(JBCopier);
 		buttonPanel.add(JBCouper);
 		buttonPanel.add(JBColler);
+		buttonPanel.add(JBDemarrer);
+		buttonPanel.add(JBArreter);
+		buttonPanel.add(JBRejouer);
 		
 		contenuFenetre.add(buttonPanel);
 		contenuFenetre.add(zoneSaisePanel);
