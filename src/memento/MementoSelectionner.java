c0ab3986fb01675f8077666ba5cebef171489f7f
ADJA GOSSAN <adjagossan@gmail.com>
2015-11-30 02:21:04 +0100

    class MementoSelectionner: int attribute added

--- src/memento/MementoSelectionner.java
+++ src/memento/MementoSelectionner.java
@@ -3,5 +3,29 @@ package memento;
 import util.IMemento;
 
 public class MementoSelectionner implements IMemento {
+	
+	private int deb;
+	private int fin;
+	
+	public MementoSelectionner(int deb, int fin)
+	{
+		this.deb = deb;
+		this.fin = fin;
+	}
 
+	public int getDeb() {
+		return deb;
+	}
+
+	public void setDeb(int deb) {
+		this.deb = deb;
+	}
+
+	public int getFin() {
+		return fin;
+	}
+
+	public void setFin(int fin) {
+		this.fin = fin;
+	}
 }
