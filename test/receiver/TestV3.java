b0adef73ccf6998186c5b1a789ddc5e87fe278cb
florentL <florent.le.b@hotmail.fr>
2015-12-09 18:02:52 +0100

    Added new tests on the undo/redo methods

--- test/receiver/TestV3.java
+++ test/receiver/TestV3.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import memento.Memento;
 
 /**
- * Classe de test sur le buffer
+ * Classe de test sur le undo/redo
  * @author Gossan Adja, Florent Le Boulch, Ammar Barry
  */
 public class TestV3 {
@@ -27,9 +27,9 @@ public class TestV3 {
 	
 	@Test
 	/**
-	 * Test undo
+	 * Test undo / redo
 	 */
-	public void testUndo() {
+	public void testUndoRedo() {
 		assertNotNull("L'objet est null lors de l'initialisation", moteur);
 		String txt = "toto";
 		Buffer buffer = moteur.getBuffer();
@@ -52,12 +52,41 @@ public class TestV3 {
 		assertEquals("Buffer différent",txt, resu);
 		
 		// redo
-		String concate = txt + txt2;
+		String concate = txt + txt2; // concate = tototiti
 		Memento mem3 = moteur.getMemento(1);
 		
 		String resu2 = mem3.getState().getBuffer().toString();
 		
 		assertEquals("Buffer différent",concate, resu2);
+		
+		// couper
+		int lenghtConcate = txt.length() + txt2.length();
+		Selection selection = new Selection(txt.length(), lenghtConcate);
+		moteur.setSelection(selection);
+		moteur.couper(); // buffer = toto
+		moteur.addMemento(new Memento(moteur.cloner())); // index = 2
+		
+		// coller
+		Selection selection2 = new Selection(0,0);
+		moteur.setSelection(selection2);
+		moteur.coller(); // buffer = tititoto
+		moteur.addMemento(new Memento(moteur.cloner())); // index = 3
+		
+		// coller
+		selection.setDebutSelection(lenghtConcate);
+		moteur.setSelection(selection);
+		moteur.coller(); // buffer = tititototiti
+		moteur.addMemento(new Memento(moteur.cloner())); // index = 4
+		
+		Memento mem4 = moteur.getMemento(2);
+		String resu3 = mem4.getState().getBuffer().toString();
+		
+		assertEquals("Buffer différent",txt, resu3);
+
+		Memento mem5 = moteur.getMemento(4);
+		String resu4 = mem5.getState().getBuffer().toString();
+		String concatDouble = txt2 + txt + txt2;
+		assertEquals("Buffer différent",concatDouble, resu4);
 		
 	}
 	
